8dbf6e92eb4d75325c7adca6a924a8debcbe05e2
vspvikram <vikramsingh9258@gmail.com>
2024-03-13 15:59:01 -0500

    updated UI

--- frontend/src/components/QuestionInput/QuestionInput.tsx
+++ frontend/src/components/QuestionInput/QuestionInput.tsx
@@ -71,7 +71,7 @@ export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend }: Pr
                     aria-label="Ask question button"
                     onClick={sendQuestion}
                 >
-                    <Send28Filled primaryFill="rgba(0, 33, 71, 1)" />
+                    <Send28Filled primaryFill="rgba(150, 0, 24, 1)" />
                     {/* <Send28Filled primaryFill="rgba(0, 157, 217, 1)" /> */}
                 </div>
             </div>
